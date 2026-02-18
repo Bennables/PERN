c2e1bf9c9d71021a6e83303c3dea663e51ca28fa
ben sirivallop <bsirival@uci.edu>
2026-02-17 17:05:01 -0800

    one way socket connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,29 +2,38 @@ import express from 'express'
 import cors from 'cors'
 import * as dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import * as http from 'node:http'
+import { WebSocketServer } from 'ws'
 
-// Import routes
 import authRoutes from './routes/auth.js'
 import taskRoutes from './routes/tasks.js'
 import orgRoutes from './routes/org.js'
 
-const app = express()
-
 dotenv.config()
+
+const app = express()
+const server = http.createServer(app)
+const wsServer = new WebSocketServer({ server })
+
+wsServer.on('connection', (socket) => {
+    console.log('WebSocket client connected')
+    socket.on('message', (data) => {
+        console.log('Received:', data.toString())
+    })
+})
+
 app.use(express.json())
 app.use(
     cors({
-        origin: 'http://localhost:5173', // Vite default port
+        origin: 'http://localhost:5173',
         credentials: true,
     })
 )
 app.use(cookieParser())
-
-// Mount routes
 app.use(authRoutes)
 app.use(orgRoutes)
 app.use(taskRoutes)
 
-app.listen(3333, () => {
+server.listen(3333, () => {
     console.log("We're connected \n http://localhost:3333")
 })
